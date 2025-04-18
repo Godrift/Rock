e24f45d24d350368086e3bbdb57166a1f15eaf13
Justin <203924655+Godrift@users.noreply.github.com>
2025-04-18 13:53:19 +0800

    rough framework done

---
+++ javascript.js
@@ -0,0 +1,57 @@
+function getComputerChoice(){
+    let value = Math.floor(Math.random() * 3);
+    switch (value){
+        case 0:
+            return "rock";
+        case 1:
+            return "scissors";
+        case 2:
+            return "paper";
+    }
+}
+
+function getHumanChoice(user){
+    return user;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    humanChoice=humanChoice.toLowerCase();
+    console.log(humanChoice, computerChoice)
+
+
+    if (humanChoice == computerChoice){
+        console.log(`Tie! Both are ${humanChoice}`)
+    }else if (computerChoice == "rock"){
+        switch (humanChoice){
+            case "scissors":
+                console.log(`You lose! Rock beats Scissors!`);
+                return 0;
+            case "paper":
+                console.log(`You win! Paper beats Rock!`);
+                return 0;
+        }
+    }else if (computerChoice == "scissors"){
+        switch (humanChoice){
+            case "paper":
+                console.log(`You lose! Scissors beats Paper!`);
+                return 0;
+            case "rock":
+                console.log(`You win! Rock beats Scissors!`);
+                return 0;
+        }
+    }else if (computerChoice == "paper"){
+        switch (humanChoice){
+            case "rock":
+                console.log(`You lose! Paper beats Rock!`);
+                return 0;
+            case "scissors":
+                console.log(`Scissors beats Paper!`);
+                return 0;
+        }
+    }
+}
+let user = "";
+playRound(getHumanChoice(prompt(user)), getComputerChoice());
